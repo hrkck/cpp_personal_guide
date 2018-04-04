bed02913979a08f2526a1c4e96350f779afcd82a
Hakki Riza <hakkirizakucuk@yandex.com>
2018-04-04 16:05:03 +0200

    demo.cpp moved to correct place. Respective paths fixed.

---
+++ 30DayOfCode/demo.cpp
@@ -0,0 +1,25 @@
+/*
+ * demo.cpp
+ *
+ *  Created on: Apr 4, 2018
+ *      Author: archer
+ */
+
+#include <iostream>
+
+#include "day1.h"
+#include "day2.h"
+using namespace std;
+
+int main() {
+
+
+	day1();
+	day2();
+
+	return 0;
+}
+
+
+// #include "../../30DayOfCode/day1.h"
+// When files are not in the same dir
