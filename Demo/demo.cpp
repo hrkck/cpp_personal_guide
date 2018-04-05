db5eb8dd95966531a3b6ec38f7abbef09dfddcc9
hrkucuk <hakkirizakucuk@yandex.com>
2018-04-05 21:06:18 +0200

    demo.cpp moved to a single folder. StringStream problem is added.

---
+++ Demo/demo.cpp
@@ -0,0 +1,33 @@
+/*
+ * demo.cpp
+ *
+ *  Created on: Apr 4, 2018
+ *      Author: archer
+ */
+
+#include <iostream>
+
+#include "../30DayOfCode/day0.h"
+#include "../30DayOfCode/day1.h"
+#include "../30DayOfCode/day2.h"
+#include "../30DayOfCode/day3.h"
+
+#include "../Strings/StringStream.h"
+
+using namespace std;
+
+int main() {
+
+
+//	day0();
+//	day1();
+//	day2();
+//	day3();
+
+	stringStreamProblem();
+	return 0;
+}
+
+
+// #include "../../30DayOfCode/day1.h"
+// When files are not in the same dir
---
+++ Strings/StringStream.cpp
@@ -0,0 +1,44 @@
+/*
+ * StringStream.cpp
+ *
+ *  Created on: Apr 5, 2018
+ *      Author: archer
+ *
+ * link to the problem:
+ * https://www.hackerrank.com/challenges/c-tutorial-stringstream/problem
+ *
+ */
+
+#include "StringStream.h"
+
+/*
+ * A great solution thanks to the great HackerRank Community:
+ *
+ * link to the comment:
+ * https://www.hackerrank.com/challenges/c-tutorial-stringstream/forum/comments/248362
+ *
+ * */
+vector<int> parseInts(string str) {
+	stringstream ss(str);
+	int a;
+	vector<int> vec;
+
+	while (ss >> a){
+	    vec.push_back(a);
+	    if (ss.peek() == ','){  ss.ignore();  }
+	}
+
+	return vec;
+}
+
+int stringStreamProblem() {
+	string str;
+	cin >> str;
+	vector<int> integers = parseInts(str);
+	for(int i = 0; i < integers.size(); i++) {
+		cout << integers[i] << "\n";
+	}
+
+	return 0;
+}
+
